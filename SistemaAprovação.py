6cf6c8aa2bdca37f77228c875f1792fb6d5fdd6c
Liliane <93098561+souzaliliane@users.noreply.github.com>
2023-03-22 02:37:59 -0300

    Create SistemaAprovação.py

---
+++ SistemaAprovação.py
@@ -0,0 +1,5 @@
+#Sistema de aprovação
+nota1 = float(input("Digite a primeira nota do aluno(a)"))
+nota2 = float(input("Digite a segunda nota"))
+media = (nota1 + nota2)/2
+print(f'Média atingida?', media >=7)
