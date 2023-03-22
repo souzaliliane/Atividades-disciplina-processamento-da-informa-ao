c754dfe0f6d0647e912fd3a698c43506a0c6461e
Liliane <93098561+souzaliliane@users.noreply.github.com>
2023-03-22 02:36:27 -0300

    Create mediaHarmonica.py

---
+++ mediaHarmonica.py
@@ -0,0 +1,7 @@
+#media Harmonica
+
+a = int(input("Digite o 1º valor para calcular a média harmônica "))
+b = int(input("Digite o 2º valor"))
+c= int (input("diite o 3º valor"))
+media = 3/(1/a + 1/b +1/c)
+print( f'Média Harmônica: {media}')
