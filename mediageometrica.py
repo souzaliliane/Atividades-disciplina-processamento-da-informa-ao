46efb349d531906fb599e73df919420ea877406e
Liliane <93098561+souzaliliane@users.noreply.github.com>
2023-03-22 02:35:24 -0300

    Create mediageometrica.py

---
+++ mediageometrica.py
@@ -0,0 +1,8 @@
+#MEDIA GEOMETRICA
+from  statistics import geometric_mean
+a = int(input("Digite o 1º número para calcular a média geometrica"))
+b = int(input('número...'))
+c = int (input("número..."))
+d = int(input("número"))
+mediag= geometric_mean([a,b,c,d])
+print (mediag)
